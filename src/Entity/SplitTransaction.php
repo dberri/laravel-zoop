f77f775c69278f6db1504bdf6f4301dd40e7b2f3
David Berri <david@segim.com.br>
2019-09-05 12:02:29 -0300

    Adds entity for split transactions

---
+++ src/Entity/SplitTransaction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DBerri\LaravelZoop\Entity;
+
+use DBerri\LaravelZoop\Entity\AbstractEntity;
+
+class SplitTransaction extends AbstractEntity
+{
+    /**
+     * id do vendedor recebedor
+     *
+     * @var string
+     */
+    public $recipient;
+
+    /**
+     * Define se o recebedor arca com prejuízo em
+     * caso de chargeback ou não. 1 arca; 0 não arca.
+     *
+     * @var boolean
+     */
+    public $liable;
+
+    /**
+     * Define se vai ser feito split em cima do valor
+     * bruto (0) ou do valor líquido (1) da transação
+     *
+     * @var boolean
+     */
+    public $charge_processing_fee;
+
+    /**
+     * Percentual da venda a ser dividido
+     *
+     * @var integer
+     */
+    public $percentage;
+
+    /**
+     * Valor em centavos a ser divido
+     *
+     * @var integer
+     */
+    public $amount;
+}
