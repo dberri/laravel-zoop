7397e6aa2a220ca6df75ca5fd35c8a5d5d5b289e
David Berri <david@segim.com.br>
2019-09-05 17:19:47 -0300

    Adds the seller_id to config variables

--- config/zoop.php
+++ config/zoop.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'marketplace_id' => env('ZOOP_MARKETPLACE_ID', ''),
+    'seller_id'      => env('ZOOP_SELLER_ID'),
     'publishable_id' => env('ZOOP_PUBLISHABLE_ID', ''),
     'url'            => env('ZOOP_URL', ''),
 ];
