3aef19eabaeac7e2f83a0f909f03343d84ee4d47
David Berri <david@segim.com.br>
2019-09-16 10:38:55 -0300

    Implements token api

---
+++ src/API/TokenApi.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DBerri\LaravelZoop\API;
+
+use DBerri\LaravelZoop\API\AbstractApi;
+use DBerri\LaravelZoop\API\Entity\BankAccount;
+use DBerri\LaravelZoop\API\Entity\Token;
+
+class TokenApi extends AbstractApi
+{
+    /**
+     * Associa conta bancaria com customer
+     *
+     * @param string $customer
+     * @param string $token
+     */
+    public function createCardToken($customer, $token)
+    {
+        return null;
+        // $response = $this->adapter->post('/bank_accounts', compact('customer', 'token'));
+        // return new BankAccount($response['body']);
+    }
+
+    /**
+     * Associa conta bancaria com customer
+     *
+     * @param BankAccount $bankAccount
+     */
+    public function createBankAccount(BankAccount $bankAccount)
+    {
+        $response = $this->adapter->post('/bank_accounts/tokens', $bankAccount->toArray());
+        return new Token($response['body']);
+    }
+}
